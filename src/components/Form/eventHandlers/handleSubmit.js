371fcce7181cdc3179f5e866107bbf222211b47a
siriusg2 <gibsonavilan@gmail.com>
2024-01-12 08:26:42 -0500

    test alert issue

--- src/components/Form/eventHandlers/handleSubmit.js
+++ src/components/Form/eventHandlers/handleSubmit.js
@@ -1,33 +1,35 @@
-
 /* eslint-disable consistent-return */
 /* eslint-disable no-alert */
 /* eslint-disable no-undef */
-import validate from './validate'
-import { createGame, updateGame } from '../../../redux/actions'
+import validate from "./validate";
+import { createGame, updateGame } from "../../../redux/actions";
 
 export const handleSubmit = async (e, form, history, isToUpdate, dispatch) => {
-  e.preventDefault()
-  validate(form)
+  e.preventDefault();
+  validate(form);
   if (!isToUpdate) {
     try {
-      const result = await dispatch((createGame(form)))
+      const result = await dispatch(createGame(form));
 
-      if (typeof result === 'string') {
-        alert(result)
+      if (typeof result === "string") {
+        alert("Game already exists");
+        alert(result);
       } else {
-        alert(`Game ${form.name} was created successfully`)
-        history.push('/home')
+        alert(`Game ${form.name} was created successfully`);
+        history.push("/home");
       }
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
+      alert("handlesubmit");
     }
   } else {
     try {
-      await dispatch(updateGame(form))
+      await dispatch(updateGame(form));
 
-      history('/home')
+      history("/home");
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
+      alert("handlesubmit2");
     }
   }
-}
+};
